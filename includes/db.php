568992b7ca6f088d3003d927c93c1939e8abbbb0
Nac Abarca <ignacioabarcanac@gmail.com>
2025-04-09 12:42:20 -0400

    modificar nombre de base de datos

--- includes/db.php
+++ includes/db.php
@@ -1,11 +1,11 @@
 <?php
-$host = "localhost";
-$user = "admin_user";
+$host = "66.45.23.20";
+$user = "marketi8_admin_user";
 $pass = "JcND3kcRmoi=";
 $db = "marketi8_dashboard";
 
 $conn = new mysqli($host, $user, $pass, $db);
 
-if ($conn->connect_error) {
-    die("❌ Error de conexión: " . $conn->connect_error);
+if(!$conn) {
+    die("❌ Error de conexión: " . mysqli_connect_error());
 }
