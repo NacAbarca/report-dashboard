a764ec7414fb030eae90af4582d6d217895ebdcf
Nac Abarca <ignacioabarcanac@gmail.com>
2025-04-11 17:07:40 -0400

    Corregir: URL: se cambio a la ruta: Vendor a CDN

--- login.php
+++ login.php
@@ -58,7 +58,7 @@ $success = $_GET['msg'] ?? '';
   </div>
 
   <!-- JS -->
-  <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
   <script type="module">
     import { notifyFromURL } from './assets/js/notifier.js';
     notifyFromURL({ mode: 'alert', containerId: 'alert-container' });
